edb6dba2c9480d9e741f099771cbc1ca779155d6
Lester Tsai <lester89217@gmail.com>
2025-06-19 00:11:30 +0800

    新增REACT_APP_API_URL

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,7 +2,8 @@
 // 它提供了簡潔的API來處理各種HTTP操作，並且在處理跨域請求方面有著重要的應用。
 //連接react和後端sever api的橋樑
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/user";
+// const API_URL = "http://localhost:8080/api/user";
+const API_URL = process.env.REACT_APP_API_URL + "/api/user";
 
 class AuthService {
   //登入
